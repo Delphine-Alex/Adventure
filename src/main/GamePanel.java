8140b2aaea3b59252341e26faebc86d383b41f77
Delphine <delphine.pva@gmail.com>
2022-03-11 15:59:55 +0100

    How to make a 2D game in Java#2 - Game loop and key input

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,24 +2,34 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel implements Runnable{
+public class GamePanel extends JPanel implements Runnable {
 	
 	// SCREEN SETTINGS
 	final int originalTileSize = 16; // 16 pixels x 16 pixels
 	final int scale = 3;
 	
-	final int titeSize = originalTileSize * scale; // 48x48 tile
+	final int tileSize = originalTileSize * scale; // 48x48 tile
 	
 	final int maxScreenColumn = 16;
 	final int maxScreenRow = 12;
-	final int screenWidth = titeSize * maxScreenColumn; // 768 pixels
-	final int screenHeight = titeSize * maxScreenRow; // 576 pixels
+	final int screenWidth = tileSize * maxScreenColumn; // 768 pixels
+	final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 	
+	// FPS
+	int FPS = 60;
 	
+	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
+	
+	// Set player's default position
+	int playerX = 100;
+	int playerY = 100;
+	int playerSpeed = 4;
 	
 	
 	public GamePanel() {
@@ -28,6 +38,9 @@ public class GamePanel extends JPanel implements Runnable{
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		// GamePanel can be focused to receive key input
+		this.setFocusable(true);
 	}
 	
 	
@@ -40,7 +53,76 @@ public class GamePanel extends JPanel implements Runnable{
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		
+		double drawInterval = 1000000000/FPS; 
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		long timer = 0;
+		int drawCount = 0;
+		
+		while(gameThread != null) {
+			
+			currentTime = System.nanoTime();
+			
+			delta += (currentTime - lastTime) / drawInterval;
+			timer += (currentTime - lastTime);
+			lastTime = currentTime;
+			
+			if(delta >= 1) {
+				// UPDATE : update information such as character positions
+				update();
+				// DRAW : draw the screen with the updated information
+				repaint();
+				delta--;
+				drawCount++;
+			}
+			
+			if(timer >= 1000000000) {
+				System.out.println("FPS:" + drawCount);
+				drawCount = 0;
+				timer = 0;
+			}
+			
+		}
+		
+		
+	}
+	
+	public void update() {
+		
+		// In Java, the upper left corner is X:O Y:O
+		// X values increases to the right
+		// Y values increases as they go down
+		
+		if(keyH.upPressed == true) {
+			playerY -= playerSpeed;
+		} 
+		else if(keyH.downPressed == true) { 
+			playerY += playerSpeed;
+		}
+		else if(keyH.leftPressed == true) { 
+			playerX -= playerSpeed;
+		}
+		else if(keyH.rightPressed == true) { 
+			playerX += playerSpeed;
+		}
+	}
+	
+	// This paint component is actually a built-in method in java
+	// Standard methods to draw things on JPanel
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		// Graphics2D class extends the Graphics class to provide more sophisticated control 
+		// over geometry, coordinate transformations, color management, and text layout
+		Graphics2D g2 = (Graphics2D)g;
+		
+		g2.setColor(Color.white);
+		
+		g2.fillRect(playerX, playerY, tileSize, tileSize);
+		
+		g2.dispose();
 		
 	}
 
